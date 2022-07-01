794b48bc53d02dc74dd8a59ba84cdeca45b19519
Sachin Acharya <acharyaraj71+github_project@gmail.com>
2022-07-01 19:55:02 +0545

    Refined Commits

--- installer.sh
+++ installer.sh
@@ -1,23 +1,42 @@
+
 #!/bin/bash
 
 RED='\033[0;31m'
 CYAN='\033[0;36m'
 
+FILENAME="assembly-language-compiler.zip"
+FOLDER="assembly-language-compiler"
+
+if [ -e "$FILENAME" ]
+then
+ echo "Required Files already exist. Deleteing existing file and Downloading new"
+ rm $FILENAME
+fi
 echo -e $CYAN"Downloading Require Files from GitHub"
 echo -e $CYAN"https://github.com/sachin-acharya-projects/assembly-language-compiler/raw/main/dist/assembly-language-compiler.zip"
 
 wget "https://github.com/sachin-acharya-projects/assembly-language-compiler/raw/main/dist/assembly-language-compiler.zip"
 
-echo -e $CYAN"Unzipping Files"
-unzip assembly-language-compiler.zip
+if [ -d "$FOLDER" ]
+then
+ echo "Installation Folder Already Exist."
+ read -p "Enter [Y/N] to delete existing folder and installing new" query
+ if [ ${query,,} = 'y' ]
+ then
+  rm -r $FOLDER
+  echo -e $CYAN"Unzipping Files"
+  unzip assembly-language-compiler.zip
 
-chmod +x assembly-language-compiler/compiler
+  chmod +x assembly-language-compiler/compiler
 rm assembly-language-compiler.zip
 
 echo -e $CYAN"Installation is complete."
 cd assembly-language-compiler
-echo "alias assembly_compiler='$PWD/compiler'" >> ~/.bashrc
-source ~/.bashrc
+ALIAS_EX=$(cat ~/.bashrc | grep "alias assembly_compiler")
+if [ "" = "$ALIAS_EX" ]
+then
+ echo "alias assembly_compiler='$PWD/compiler'" >> ~/.bashrc
+ source ~/.bashrc
 echo -e $CYAN"Run assembly_compiler [filename] to compiler"
 echo -e $CYAN"If assembly_compiler: command not found error occured, please run the following command or restart the computer."
 echo -e $CYAN"source ~/.bashrc"
