720236340beb331b1d6697f2f7678fecd4d24eb6
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2022-07-01 20:21:50 +0545

    Updated

--- installer.sh
+++ installer.sh
@@ -19,26 +19,31 @@ wget "https://github.com/sachin-acharya-projects/assembly-language-compiler/raw/
 
 if [ -d "$FOLDER" ]
 then
- echo "Installation Folder Already Exist."
- read -p "Enter [Y/N] to delete existing folder and installing new" query
- if [ ${query,,} = 'y' ]
- then
-  rm -r $FOLDER
-  echo -e $CYAN"Unzipping Files"
-  unzip assembly-language-compiler.zip
-  chmod +x assembly-language-compiler/compiler
- fi
+    echo "Installation Folder Already Exist."
+    read -p "Enter [Y/N] to delete existing folder and installing new" query
+    if [ ${query,,} = 'y' ]
+    then
+        rm -r $FOLDER
+        echo -e $CYAN"Unzipping Files"
+        unzip assembly-language-compiler.zip
+        chmod +x assembly-language-compiler/compiler
+    fi
 fi
+
 rm assembly-language-compiler.zip
 
 echo -e $CYAN"Installation is complete."
+
 cd assembly-language-compiler
+
 ALIAS_EX=$(cat ~/.bashrc | grep "alias assembly_compiler")
+
 if [ "" = "$ALIAS_EX" ]
 then
  echo "alias assembly_compiler='$PWD/compiler'" >> ~/.bashrc
  source ~/.bashrc
 fi
+
 echo -e $CYAN"Run assembly_compiler [filename] to compiler"
 echo -e $CYAN"If assembly_compiler: command not found error occured, please run the following command or restart the computer."
 echo -e $CYAN"source ~/.bashrc"
