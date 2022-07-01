41c0ac11b94098b471b618f6ffd669aa0f14b24e
Sachin Acharya <51048248+sachin-acharya-projects@users.noreply.github.com>
2022-07-01 15:04:22 +0545

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -18,3 +18,5 @@ echo -e $CYAN"Installation is complete."
 cd assembly-language-compiler
 echo "alias assembly_compiler='$PWD/compiler'" >> ~/.bashrc
 source ~/.bashrc
+echo -e $CYAN"Run assembly_compiler [filename] to compiler"
+echo -e $CYAN"If assembly_compiler: command not found error occured, please restart the computer."
